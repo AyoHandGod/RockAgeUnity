cb601cb8adad0f2659106c65b886b3b9a448f2af
Dayo <admin@nerdroco.com>
2018-11-06 17:22:55 -0800

    Initial Commit
    Pretty much complete. Can be used to play around with and add additional features.

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour { 
+
+    public float speed = .75f;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        const float MinImpulseForce = 1f;
+        const float MaxImpulseForce = 3f;
+
+        float angle = Random.Range(0, 2 * Mathf.PI);
+
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+
+        float magnitude = Random.Range(MinImpulseForce,
+            MaxImpulseForce);
+
+        GetComponent<Rigidbody2D>().AddForce(direction * magnitude,
+            ForceMode2D.Impulse);
+    }
+
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour {
+
+    #region Fields
+    [SerializeField]
+    GameObject prefabRock;
+
+    GameObject[] rocksInScene;
+
+    [SerializeField]
+    Sprite[] sprites;
+    Timer spawnTime;
+
+    // Screen size settings
+    int SpawnBorderSize = 100;
+    int minSpawnX;
+    int maxSpawnX;
+    int minSpawnY;
+    int maxSpawnY;
+
+    #endregion
+
+    #region Methods
+    // Use this for initialization
+    void Start () {
+        // Set the screen points
+        minSpawnX = SpawnBorderSize;
+        maxSpawnX = Screen.width - SpawnBorderSize;
+        minSpawnY = SpawnBorderSize;
+        maxSpawnY = Screen.height - SpawnBorderSize;
+
+        spawnTime = gameObject.AddComponent<Timer>();
+        spawnTime.Duration = 3;
+        spawnTime.Run();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rocksInScene = GameObject.FindGameObjectsWithTag("rock");
+        if (spawnTime.Finished)
+        {
+            if(rocksInScene.Length < 3)
+            {
+                spawnRock();
+            }
+            spawnTime.Run();
+        }
+	}
+
+    /// <summary>
+    /// Spawn Rock GameObject and assigns sprite
+    /// </summary>
+    void spawnRock()
+    {
+        // generate random Location for Rock and translate to worldLocation
+        Vector3 location = new Vector3(Random.Range(minSpawnX, maxSpawnX),
+            Random.Range(minSpawnY, maxSpawnY),
+            -Camera.main.transform.position.z);
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+
+        // spawn rock using location
+        GameObject rock = Instantiate(prefabRock) as GameObject;
+        rock.transform.position = worldLocation;
+
+        // Set the sprite for the rock
+        SpriteRenderer spriteRenderer = rock.GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, sprites.Length);
+        spriteRenderer.sprite = sprites[spriteNumber];
+    }
+
+    #endregion
+}
